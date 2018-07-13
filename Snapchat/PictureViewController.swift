4bdd65676a7999e8817c1003e5a8794c386ecafe
gofordonuts <andrew.beutel@gmail.com>
2018-07-13 08:03:34 -0500

    Firebase Auth implementation
    Added firebase for authenticating users based on email link

---
+++ Snapchat/PictureViewController.swift
@@ -0,0 +1,50 @@
+//
+//  PictureViewController.swift
+//  Snapchat
+//
+//  Created by William Beutel on 7/11/18.
+//  Copyright © 2018 Maria Beutel. All rights reserved.
+//
+
+import UIKit
+
+class PictureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var decriptionTextField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    var imagePicker = UIImagePickerController()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = image
+        imageView.backgroundColor = UIColor.clear
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = false
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func nextTapped(_ sender: Any) {
+    }
+    
+
+}
---
+++ Snapchat/SignInViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SignInViewController.swift
+//  Snapchat
+//
+//  Created by William Beutel on 7/7/18.
+//  Copyright © 2018 Maria Beutel. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func turnUpTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            if error != nil {
+                print("Error signing into existing account: \(String(describing:error))")
+                Auth.auth().createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: { (user, error) in
+                    if error != nil {
+                        print("Error creating new account: \(String(describing: error))")
+                    } else {
+                        print("Successfully created new account")
+                        self.performSegue(withIdentifier: "signInSegue", sender: nil)
+                    }
+                })
+            } else {
+                print("Successfully logged into existing account")
+                self.performSegue(withIdentifier: "signInSegue", sender: nil)
+                
+            }
+        }
+    }
+    
+}
